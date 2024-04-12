909aeb6405679cb3c0f64cfbc39a441c4adbc209
yanagitani <takanori.yanagitani@gmail.com>
2024-04-13 07:03:51 +0900

    test: coverage shell for macOS

--- cov.sh
+++ cov.sh
@@ -3,6 +3,8 @@
 numcpus=23
 jobs=$((${numcpus} - 1))
 
+prefix=./.build/debug/FloatToDataPackageTests.xctest
+
 covLinux() {
 	swift \
 		test \
@@ -30,7 +32,29 @@ covLinux() {
 }
 
 covDarwin() {
-  exec sh -c 'echo TODO: not implemented for macOS; exit 1'
+	swift \
+		test \
+		--quiet \
+		--parallel \
+		--enable-code-coverage \
+		--jobs ${jobs} ||
+		exec sh -c 'echo TEST FAILURE; exit 1'
+
+	xcrun llvm-cov \
+		export \
+		-format=lcov \
+		"${prefix}/Contents/MacOS/FloatToDataPackageTests" \
+		-instr-profile ./.build/debug/codecov/default.profdata |
+		cat >./cov.lcov
+
+	xcrun llvm-cov \
+		report \
+		--ignore-filename-regex=.build \
+		--summary-only \
+		"${prefix}/Contents/MacOS/FloatToDataPackageTests" \
+		-instr-profile ./.build/debug/codecov/default.profdata
+
+	exit $?
 }
 
 case $(uname -o) in
